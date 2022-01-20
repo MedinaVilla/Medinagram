abee00f36fa88d89fab21289c0d76490133c0d98
MedinaVilla <net_medina@hotmail.com>
2022-01-20 14:01:17 -0600

    Initial configuration to work

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Footer: React.FC<{}> = ()=>{
+    return <footer>
+        Realizado por: Medina Villa 2022
+    </footer>
+
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Navbar: React.FC<{}> = () => {
+    return <nav>
+        <ul>
+            <li>
+                Inicio
+            </li>
+            <li>
+                Mensajes
+            </li>
+            <li>
+                Perfil
+            </li>
+        </ul>
+    </nav>
+
+}
