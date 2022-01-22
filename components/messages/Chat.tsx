3da0c9cbc1de78b1be85b26e9a61821f6c757082
MedinaVilla <net_medina@hotmail.com>
2022-01-22 17:29:28 -0600

    Messages, chat almost ready, functionality remains

--- components/messages/Chat.tsx
+++ components/messages/Chat.tsx
@@ -1,9 +1,62 @@
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./../../styles/Chat.module.css";
 
-export const Chat: React.FC<any> = ({ chatWith }) => {
+interface IChat {
+    messageDay: {
+        date: string
+        messages:
+        {
+            me: boolean
+            message: string
+        }[]
 
+    }[]
+}
+
+export const Chat: React.FC<any> = ({ chatWith }) => {
+    const [chat, setChat] = useState<IChat>();
+    const [input, setInput] = useState<string>("")
+
+    useEffect(() => {
+        setChat({
+            messageDay: [
+                {
+                    date: "6 de Enero de 2022",
+                    messages: [{
+                        me: true,
+                        message: "Oye y como has estado brother"
+                    },
+                    {
+                        me: false,
+                        message: "Bien aquí con la familia ya sabes, y tú como has estado???"
+                    },
+                    {
+                        me: false,
+                        message: "Por cierto, feliz año nuevo 2022"
+                    },
+                    {
+                        me: true,
+                        message: "Muy bien, gracias!!! Igualmente"
+                    }]
+                },
+                {
+                    date: "8 de Enero de 2022",
+                    messages: [{
+                        me: false,
+                        message: "Te quiero mucho, y lo sabes"
+                    },
+                    {
+                        me: true,
+                        message: "Jajajaja si, si lo sé :3"
+                    }
+                    ]
+                }
+            ]
+
+        })
+
+    }, [])
 
     if (chatWith)
         return (
@@ -27,7 +80,37 @@ export const Chat: React.FC<any> = ({ chatWith }) => {
                     </div>
                 </div>
                 <div className={styles.containerChat}>
-                    Mi chat con {chatWith.nickname}
+                    {
+                        chat && chat.messageDay.length > 0 && chat.messageDay.map((messagesDay, index: number) => {
+                            return <div key={index}>
+                                <p className={styles.dateChat}><small>{messagesDay.date}</small></p>
+                                {
+                                    messagesDay.messages.length > 0 && messagesDay.messages.map((message, index: number) => {
+                                        return <div className={styles.messageContainer} key={index}>
+                                            <span className={`${styles.messageChat} ${message.me ? styles.messageMe : styles.messageFriend}`}>
+                                                {message.message}
+                                                </span>
+                                        </div>
+                                    })
+                                }
+                            </div>
+                        })
+
+                    }
+                    <div className={styles.inputContainer}>
+                        <div style={{cursor:"pointer"}}>
+                            <Image src="/assets/smile.png" width={28} height={28} alt="home" />
+                        </div>
+                        <input className={styles.input} placeholder="Enviar un mensaje..." value={input} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setInput(e.target.value) }} />
+                        <div className={styles.iconInput}>
+                            <div>
+                                <Image src="/assets/image.png" width={32} height={32} alt="home" />
+                            </div>
+                            <div>
+                                <Image src="/assets/heart.png" width={32} height={32} alt="home" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         ); else {
