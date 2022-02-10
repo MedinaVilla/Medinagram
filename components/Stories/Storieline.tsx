3e7ab7520b7b8ebcab55ac9befa821234ba6b9ac
MedinaVilla <net_medina@hotmail.com>
2022-02-09 23:44:56 -0600

    Storyline ready, implementend mechanism carousel

--- components/Stories/Storieline.tsx
+++ components/Stories/Storieline.tsx
@@ -3,39 +3,45 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./../../styles/Storyline.module.css";
 
 
-export const Storieline: React.FC<any> = ({ goNextSlide, story, page, index }) => {
+export const Storieline: React.FC<any> = ({ changeStory, goNextSlide, story, page, index, updateMiniStory }) => {
 
-    const progressContainer = useRef<HTMLDivElement>(null);
-    const status = useRef<HTMLDivElement>(null);
     const progress = useRef<any>();
     const [actualStory, setActualStory] = useState(0);
-    const [view, setView] = useState(false);
     const [play, setPlay] = useState(true);
-
-    const clickHandler = (e: any) => {
-        if (progressContainer && progressContainer.current) {
-            const index = Math.floor(e.offsetX / (progressContainer.current.clientWidth / 3));
-            if (status && status.current) {
-                status.current.innerText = "Clicked " + index;
-            }
-        }
-    }
-
-    useEffect(() => {
-        if (progressContainer && progressContainer.current)
-            progressContainer.current.addEventListener("click", clickHandler, false);
-    })
 
     const intervalRef = useRef<any>();
 
+    const useHasChanged = (val: any) => {
+        const prevVal = usePrevious(val)
+        if (prevVal === undefined) return null; else
+            return prevVal! < val;
+    }
+
+    const usePrevious = (value: any) => {
+        const ref = useRef();
+        useEffect(() => {
+            ref.current = value;
+        });
+        return ref.current;
+    }
+
+    const goToRightDirection = useHasChanged(changeStory)
+
     useEffect(() => {
-        if (play === true && page == index) {
+        if (play === true && page == index && actualStory >= 0) {
             let a = setInterval(function () {
                 if (actualStory < progress.current.length) {
                     progress.current[actualStory].value = progress.current[actualStory].value + 1;
                     if (progress.current[actualStory].value == 100) {
-                        setActualStory(actualStory + 1);
-                        clearInterval(a);
+                        if (actualStory == progress.current.length - 1) {
+                           
+                            clearInterval(a);
+                            goNextSlide(true);
+                        } else {
+                            updateMiniStory();
+                            setActualStory(actualStory + 1);
+                            clearInterval(a);
+                        }
                     }
                 }
             }, 50);
@@ -43,21 +49,45 @@ export const Storieline: React.FC<any> = ({ goNextSlide, story, page, index }) =
         } else clearInterval(intervalRef.current);
     }, [actualStory, index, page, play])
 
-    useEffect(() => {
-        if (page == index)
-            setView(true)
-        else
-            setView(false);
-    }, [index, page])
 
     useEffect(() => {
-        if (actualStory == story.image.length){
-            progress.current[actualStory-1].value = 0;
-            setActualStory(actualStory-1);
-            goNextSlide();
+        if (page == index) { // Se encuentra activa la historia
+            if (changeStory < progress.current.length && changeStory >= 0) {
+                if (actualStory + 1 === progress.current.length) {
+                    if (goToRightDirection) {
+                        progress.current[actualStory].value = 0;
+                    } else {
+                        progress.current[actualStory].value = 0;
+                        progress.current[actualStory - 1].value = 0;
+                        clearInterval(intervalRef.current);
+                        setActualStory(actualStory - 1)
+                    }
+                } else
+                    if (goToRightDirection) {
+                        progress.current[actualStory].value = 100;
+                    } else {
+                        if (actualStory > 0) {
+                            progress.current[actualStory].value = 0;
+                            progress.current[actualStory - 1].value = 0;
+                            clearInterval(intervalRef.current);
+                            setActualStory(actualStory - 1)
+                        }
+                    }
+            } else {
+                if (changeStory === progress.current.length) {
+                    progress.current[actualStory].value = 100;
+                    goNextSlide(true);
+                } else {
+                    progress.current[actualStory].value = 0;
+                    clearInterval(intervalRef.current);
+                    goNextSlide(false);
+
+                }
+            }
 
         }
-    }, [actualStory, goNextSlide, story.image.length])
+    }, [changeStory, index, page,])
+
 
     const storiesPausedPlay = () => {
         if (play === true) {
@@ -68,12 +98,13 @@ export const Storieline: React.FC<any> = ({ goNextSlide, story, page, index }) =
         }
     }
 
+
     return (
         <div className={styles.itemCarousel}>
             <div style={{ backgroundImage: `url(${story.image[actualStory]})`, height: "500px", backgroundRepeat: "no-repeat", backgroundSize: "cover" }} >
                 {/* <Image src="/assets/storyline1.jfif" width={500} height={500} alt="as" /> */}
                 <div className={styles.backgroundHeader}>
-                    <div ref={progressContainer} className={styles.progressContainer}>
+                    <div className={styles.progressContainer}>
                         {story.image.length > 0 && story.image.map((image: any, index: number) => {
                             progress.current = []
                             return <progress key={index} ref={el => el ? progress.current[index] = el : ""} className={`${styles.progress}`} value="0" max="100"></progress>
@@ -81,7 +112,7 @@ export const Storieline: React.FC<any> = ({ goNextSlide, story, page, index }) =
                     </div>
                     <div className={styles.headerStory}>
                         <div className={styles.profile}>
-                            <div style={{ width: "42px", height: "42px", borderRadius: "50%", overflow: 'hidden', position: "relative" }}>
+                            <div style={{ width: "36px", height: "36px", borderRadius: "50%", overflow: 'hidden', position: "relative" }}>
                                 <Image src="https://instagram.fpbc6-1.fna.fbcdn.net/v/t51.2885-19/s320x320/192652294_2862989684011510_3685363033691202201_n.jpg?_nc_ht=instagram.fpbc6-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=ZIEVOGYpSUIAX8hTrTa&edm=ABfd0MgBAAAA&ccb=7-4&oh=00_AT96lC7REFqnj90RLh8_mq-LG5AkxwwYq9QZMleCvQQrzQ&oe=6207EABA&_nc_sid=7bff83" layout="fill" alt="home" />
                             </div>
                             <div className={styles.nickname}>
@@ -93,13 +124,13 @@ export const Storieline: React.FC<any> = ({ goNextSlide, story, page, index }) =
                         </div>
                         <div className={styles.icons}>
                             <div>
-                                <Image onClick={() => storiesPausedPlay()} src="/assets/pause.png" width={28} height={28} alt="compass" />
+                                <Image onClick={() => storiesPausedPlay()} src="/assets/pause.png" width={24} height={24} alt="compass" />
                             </div>
                             <div>
-                                <Image src="/assets/speaker.png" width={24} height={22} alt="compass" />
+                                <Image src="/assets/speaker.png" width={20} height={17} alt="compass" />
                             </div>
                             <div>
-                                <Image src="/assets/three-points.png" width={28} height={28} alt="compass" />
+                                <Image src="/assets/three-points.png" width={24} height={24} alt="compass" />
                             </div>
                         </div>
                     </div>
--- pages/stories/[user]/[id]/index.tsx
+++ pages/stories/[user]/[id]/index.tsx
@@ -6,12 +6,13 @@ import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import slickNext from "react-slick";
 import { Storieline } from "../../../../components/Stories/Storieline";
 
 
 const Storyline: NextPage = () => {
     const [actualPage, setActualPage] = useState(0);
+    const [miniStoryPosition, setMiniStoryPosition] = useState<number>(0);
+
     const sliderRef = useRef<any>();
 
     const [storiesUser, setStoriesUser] = useState([
@@ -23,9 +24,10 @@ const Storyline: NextPage = () => {
             image: ["/assets/storyline2.png", "/assets/storyline1.jfif", "/assets/storyline3.jpg"]
         },
         {
-            id: "1623467437345743574",
+            id: "162346743734d5743574",
             image: ["/assets/storyline3.jpg", "/assets/storyline2.png", "/assets/storyline1.jfif"]
-        }
+        },
+       
     ])
 
     var settings = {
@@ -34,29 +36,56 @@ const Storyline: NextPage = () => {
         infinite: false,
         speed: 300,
         slidesToShow: 1,
-        focusOnSelect: true,
-
-        centerPadding: '500px',
+        // focusOnSelect: true,
+        arrows: false,
         draggable: false,
         variableWidth: true,
         adaptiveHeight: true,
-        beforeChange: (pagePrev: number, pageNew: number) => { setActualPage(pageNew) }
     };
+
+    const beforeChange = (rightDirection: boolean) => {
+        if (rightDirection) {
+            setMiniStoryPosition(0);
+            setActualPage(actualPage + 1)
+            sliderRef.current.slickNext()
+        } else {
+            setMiniStoryPosition(storiesUser.length - 1);
+            sliderRef.current.slickPrev();
+            setActualPage(actualPage - 1)
+        }
+    }
+
+    const updateMiniStory = () => {
+            setMiniStoryPosition(miniStoryPosition + 1);
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.header}>
-                <Image src="/assets/logoMBlack.png" width="125" height="35" alt="logo" />
-                X
+                <Image className={styles.logo} src="/assets/logoMBlack.png" width="125" height="35" alt="logo" />
+                <div style={{ cursor: "pointer" }}>X</div>
             </div>
             <div>
                 <div className={styles.body}>
-
                     <Slider ref={sliderRef}  {...settings}>
                         {storiesUser.length > 0 && storiesUser.map((story, index) => {
-                            return <React.Fragment key={index}><Storieline goNextSlide={() => { sliderRef.current.slickNext() }} story={story} page={actualPage} index={index} /> </React.Fragment>
+                            return <React.Fragment key={index}
+                            ><Storieline changeStory={miniStoryPosition} goNextSlide={(value: boolean) => { beforeChange(value) }} story={story} page={actualPage} index={index} updateMiniStory={updateMiniStory} />
+                            </React.Fragment>
                         })}
                     </Slider>
+                </div>
+                <div style={{ textAlign: "center" }}>
+                    {actualPage === 0 && miniStoryPosition === 0 ? "" :
+                        <div onClick={() => { setMiniStoryPosition(miniStoryPosition - 1) }} style={{ cursor: "pointer", width: 30, height: 30, position: "absolute", top: "400px", left: "540px" }}>
+                            <Image src="/assets/left.png" layout="fill" alt="right" />
+                        </div>
+                    }
+                    {actualPage === storiesUser.length - 1 && miniStoryPosition === storiesUser[storiesUser.length - 1].image.length - 1 ? "" :
+                        <div onClick={() => { setMiniStoryPosition(miniStoryPosition + 1) }} style={{ cursor: "pointer", width: 30, height: 30, position: "absolute", top: "400px", right: "540px" }}>
+                            <Image src="/assets/right.jpg" layout="fill" alt="right" />
+                        </div>
+                    }
                 </div>
             </div>
         </div >
